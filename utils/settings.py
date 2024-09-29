ac99f3d252ab624a77ae7b6f1dd006616a42c01f
Luka Colić <lcenzo@protonmail.ch>
2024-09-29 20:59:29 +0200

    Add settings via env variables

---
+++ utils/settings.py
@@ -0,0 +1,23 @@
+import enum
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Modes(enum.Enum):
+    PROD = "PROD"
+    DEBUG = "DEBUG"
+
+
+class Settings(BaseSettings):
+    mode: Modes = Modes.PROD
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+    def is_debug(self) -> bool:
+        return self.mode == Modes.DEBUG
+
+    def is_prod(self) -> bool:
+        return self.mode == Modes.PROD
+
+
+settings = Settings()
