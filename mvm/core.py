feaf4748f5877ec9c0f975b97e2c996da908658d
Luka Colić <lcenzo@protonmail.ch>
2024-10-14 22:17:23 +0200

    Fix combatants.py to use new state machine logic

--- mvm/core.py
+++ mvm/core.py
@@ -61,6 +61,8 @@ class HitRollData:
 @dataclass
 class DamageData:
     attacker_is_a: bool
+    pre_damage_armor: int
+    pre_damage_shields: int
     damage: int
 
 
@@ -406,11 +408,19 @@ class AttackState(TurnState, ABC):
         does_hit: bool = self.calculate_hit(attacker, defender)
         if does_hit:
             damage = self.get_damage(attacker, defender)
-            self.apply_effects(Signal(SignalType.POST_DMG_CALC, DamageData(self.a_is_attacking, damage)))
+            pre_damage_armor = defender.armor
+            pre_damage_shields = defender.shields
+            self.apply_effects(Signal(
+                SignalType.POST_DMG_CALC, 
+                DamageData(self.a_is_attacking, pre_damage_armor, pre_damage_shields, damage)
+            ))
             damage = DamageData.damage
 
             applied_damage = defender.apply_damage(damage, att_type)
-            self.apply_effects(Signal(SignalType.POST_DMG_APPLICATION, DamageData(self.a_is_attacking, applied_damage)))
+            self.apply_effects(Signal(
+                SignalType.POST_DMG_APPLICATION, 
+                DamageData(self.a_is_attacking, pre_damage_armor, pre_damage_shields, damage)
+            ))
             logger.info(f"{attacker.name} hits {defender.name} with {att_type.value} for {applied_damage} damage")
         else:
             logger.info(f"{attacker.name} misses {defender.name} with {att_type.value}")
